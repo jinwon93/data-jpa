593aa1802183015cd331c07b1c3cc68f3c9f3696
jinwon93 <kjw8208@nate.com>
2021-10-26 22:01:40 +0900

    memberteam

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,29 +1,41 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id" , "username" , "age"})
 public class Member {
 
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private  Long id;
     private String userName;
+    private  int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
 
-    protected Member(){
-
-    }
+//    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+//    teamprotected Member(){
+//
+//    }
 
     public Member(String userName){
         this.userName = userName;
+    }
+
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
