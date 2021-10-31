a44d18db94d825d24084f9035a4d870897a796cf
jinwon93 <kjw8208@nate.com>
2021-10-31 20:16:14 +0900

    age

--- data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -4,6 +4,9 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team , Long> {
 
+    List<Team> findByTeem(String team);
 }
