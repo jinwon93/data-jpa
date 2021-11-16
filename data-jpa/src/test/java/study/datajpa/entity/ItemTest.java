d8476866a51d6149fb43c6c02c4a5c127dd56cf8
jinwon93 <kjw8208@nate.com>
2021-11-16 22:39:41 +0900

    item

---
+++ data-jpa/src/test/java/study/datajpa/entity/ItemTest.java
@@ -0,0 +1,22 @@
+package study.datajpa.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.repository.ItemRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class ItemTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    public void save(){
+        Item item = new Item();
+        itemRepository.save(item);
+    }
+}
